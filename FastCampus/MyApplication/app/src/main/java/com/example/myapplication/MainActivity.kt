a963306473041d0e527aa8fee3e7ea82bb1b4cda
mk <pinch24@gmail.com>
2020-04-14 16:23:02 +0900

    Update MainActivity.kt

--- FastCampus/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ FastCampus/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.n11_final)
+
+        Log.d("LIFE CYCLE", "onCreate")
+    }
+
+    override fun onStart() {
+
+        super.onStart()
+
+        Log.d("LIFE CYCLE", "onStart")
+    }
+
+    override fun onResume() {
+
+        super.onResume()
+
+        Log.d("LIFE CYCLE", "onResume")
+    }
+
+    override fun onPause() {
+
+        super.onPause()
+
+        Log.d("LIFE CYCLE", "onPause")
+    }
+
+    override fun onStop() {
+
+        super.onStop()
+
+        Log.d("LIFE CYCLE", "onStop")
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+
+        Log.d("LIFE CYCLE", "onDestroy")
     }
 }
