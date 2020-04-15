89e734cc5832f0d19c36ca9ddfdc6aadab56de79
mk <pinch24@gmail.com>
2020-04-15 12:24:22 +0900

    Intent

---
+++ FastCampus/MyApplication/app/src/main/java/com/example/myapplication/Intent1.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_intent1.*
+
+class Intent1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent1)
+
+        change_activity.setOnClickListener {
+
+            var intent = Intent(this@Intent1, Intent2::class.java)
+            intent.apply {
+
+                this.putExtra("number1", 1)
+                this.putExtra("number2", 2)
+            }
+            startActivityForResult(intent, 200)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 200) {
+
+            Log.d("request_code", "" + requestCode)
+            Log.d("result_code", "" + resultCode)
+
+            var result = data?.getIntExtra("result", 0)
+            Log.d("result", "" + result)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+}
